0e90b3a45b9dfd50e3793504d4e99825c58a89c4
Robert Ahlberg <rob.ahlberg@gmail.com>
2019-02-11 21:49:42 +0100

    Phaser with Parcel packager

---
+++ README.md
@@ -0,0 +1,16 @@
+# Start for <Hockair Project> 🎮
+
+## Features
+
+- Parcel
+- ESLint and Prettier
+
+## Getting started
+
+💻 `npm start`
+
+🔨 `npm run build`
+
+❌ `npm run lint`
+
+💄 `npm run pretty`
---
+++ src/scenes/game.js
@@ -0,0 +1,10 @@
+import Phaser from 'phaser';
+import constants from '../config/constants';
+
+class Game extends Phaser.Scene {
+  preload() {}
+  create() {}
+  update() {}
+}
+
+export default Game;
